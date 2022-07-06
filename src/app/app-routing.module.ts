a6253670b1df11643f63d58cc937fe8bc56df08c
Chiheb Mezrigui <cmezriguichiheb@gmail.com>
2022-07-06 17:21:13 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PortoflioComponent } from './portoflio/portoflio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'por?tfoliofor?chiheb',component:HomeComponent},
+  {path:'about?chihebmezrigui',component:AboutComponent},
+  {path:'Work?chihebmezrigui?projects',component:PortoflioComponent},
+  {path:'contact?contactmezrigui',component:ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
